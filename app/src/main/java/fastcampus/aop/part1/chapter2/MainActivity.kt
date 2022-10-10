0512b70a465ca9382068209e4d205bebbda60577
ethanchae <boschae36@gmail.com>
2022-10-10 17:42:00 +0900

    feat : 기능 구현

--- app/src/main/java/fastcampus/aop/part1/chapter2/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part1/chapter2/MainActivity.kt
@@ -1,11 +1,25 @@
 package fastcampus.aop.part1.chapter2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import fastcampus.aop.part1.chapter2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.convertButton.setOnClickListener {
+            val number = binding.editText.text.toString().toInt()
+
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("cmValue", number)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/fastcampus/aop/part1/chapter2/ResultActivity.kt
+++ app/src/main/java/fastcampus/aop/part1/chapter2/ResultActivity.kt
@@ -2,10 +2,17 @@ package fastcampus.aop.part1.chapter2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import fastcampus.aop.part1.chapter2.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val cmValue = intent.getIntExtra("cmValue", 0)
+        binding.textView.text = (cmValue / 100f).toString()
     }
 }
